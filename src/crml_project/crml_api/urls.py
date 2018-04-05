354c3c294cf7f8f610b5d2481ad80ab44978d83c
irvingylk <irvingylk@hotmail.com>
2018-04-05 22:27:41 +0930

    add new url for post

--- src/crml_project/crml_api/urls.py
+++ src/crml_project/crml_api/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
 
     url(r'^rawcomment-list-all-view/$', views.RawCommentItemsApiView.as_view()),
+    url(r'^rawcomment-view/$', views.RawCommentItemApiView.as_view()),
     url(r'^rawcomment-view/(?P<id>[a-zA-Z0-9]+)$', views.RawCommentItemApiView.as_view())
 ]
