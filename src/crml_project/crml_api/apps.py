43057c385aa9ad8bbd7f5b4f711e783f74f24ecc
irvingylk <irvingylk@hotmail.com>
2018-05-08 03:31:50 +0930

    remove src

--- src/crml_project/crml_api/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CrmlApiConfig(AppConfig):
-    name = 'crml_api'
--- src/crml_project/crml_api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-
-
-urlpatterns = [
-    url(r'^review/$', views.ReviewApiView.as_view()),
-    url(r'^review/(?P<id>[a-zA-Z0-9]+)$', views.ReviewApiView.as_view())
-]
