ae706c4e372ebb58012c2fd10caec0b9d9be7e7c
irvingylk <irvingylk@hotmail.com>
2018-05-08 19:43:31 +0930

    modified

---
+++ src/crml_project/scripts/init_tags.py
@@ -0,0 +1,13 @@
+from crml_api.models import *
+
+
+def run(*args):
+
+    tags = ['Check', 'Solution Approach', 'Organization', 'Support By Language', 'Textual', 'Larger Defects', 'Support', 'Resource', 'Logic', 'Interface', 'Traceability', 'Process']
+
+    for i in range(len(tags)):
+
+        try:
+            Tag.objects.create(tagId=i+1, description=tags[i])
+        except:
+            print('error%d'%(i+1))
