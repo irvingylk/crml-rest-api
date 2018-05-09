03f01ac198cd4e826a27a4cedab4880e6e99b61d
irvingylk <irvingylk@hotmail.com>
2018-05-10 04:08:15 +0930

    fix bugs trigger by no features on a review

--- src/crml_project/scripts/extract_features.py
+++ src/crml_project/scripts/extract_features.py
@@ -1,16 +1,25 @@
 from crml_api.models import *
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, ENGLISH_STOP_WORDS
 from decimal import Decimal
 #import pickle
 
 def run(*args):
 
     reviews = Review.objects.filter(reviewed=True, extracted=False)
-    vectorizer = CountVectorizer(strip_accents='ascii')
+    vectorizer = CountVectorizer(strip_accents='ascii', stop_words=ENGLISH_STOP_WORDS)
 
     for r in reviews:
 
-        tf_list = vectorizer.fit_transform([r.review_content]).toarray()[0]
+        try:
+            a = vectorizer.fit_transform([r.review_content]).toarray()
+        except:
+            continue
+        
+
+        if len(a) == 0:
+            continue
+
+        tf_list = a[0]
         features = vectorizer.get_feature_names()
         r.extracted = True
         r.save()
